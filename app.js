42506b1e20ad7b81ecac62503c71999a51ff8a40
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-04-22 15:48:26 +0200

    [UPD] Created and setup app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+//Basic Express.JS start
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+
+//Routing
+const postsRouter = require('./routers/posts.js');
+app.use('/posts', postsRouter);
+
+//Base Route
+app.get('/', (req, res) => {
+    res.send('Hello Boolean!');
+});
+
+//SET Port listener
+app.listen(port, () => {
+    console.log(`Server listening to http://localhost:${port}`);
+});
